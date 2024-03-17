8841d9b4d24f19ce49fd7c66a8bdc6c9b64adb7b
Pisaric <116560852+Pisaric@users.noreply.github.com>
2024-03-17 23:47:49 +0100

    crud encounters

--- src/Explorer.API/Controllers/Administrator/Administration/EncounterController.cs
+++ src/Explorer.API/Controllers/Administrator/Administration/EncounterController.cs
@@ -4,8 +4,13 @@ using Explorer.Encounters.API.Public;
 using Explorer.Encounters.Core.UseCases.Tourist.Execution;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain.Tours;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace Explorer.API.Controllers.Administrator.Administration
 {
@@ -20,12 +25,43 @@ namespace Explorer.API.Controllers.Administrator.Administration
             _encountersService = encountersService;
         }
 
+
+        //OVO MENJAM
         [HttpGet]
-        public ActionResult<PagedResult<EncounterDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        public async Task<ActionResult<PagedResult<EncounterStringDto>>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
         {
-            var result = _encountersService.GetAllCheckpointUnrelated();
-            return CreateResponse(result);
+        //   var result = _encountersService.GetAllCheckpointUnrelated();
+        //    return CreateResponse(result);
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "http://localhost:8083/api/encounters";
+                try
+                {
+                    var response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var responseData = await response.Content.ReadFromJsonAsync<List<EncounterStringDto>>();
+                        var pagedResult = new PagedResult<EncounterStringDto>(responseData, responseData.Count);
+
+                        return Ok(pagedResult);
+
+
+                    }
+                    else
+                    {
+                        return StatusCode((int)response.StatusCode, "Error calling the Spring microservice");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    return StatusCode(500, $"Request to microservice failed: {ex.Message}");
+                }
+            }
+        
         }
+        //OVO MENJAM
 
         [HttpGet("statistics/{encounterId:long}")]
         public ActionResult<EncounterStatisticsDto> GetStatistics(long encounterId)
@@ -48,25 +84,101 @@ namespace Explorer.API.Controllers.Administrator.Administration
             return CreateResponse(result);
         }
 
+        // OVO MENJAM
         [HttpPost]
-        public ActionResult<EncounterDto> Create([FromBody] EncounterDto encounter)
+        public async Task<ActionResult<EncounterStringDto>> Create([FromBody] EncounterStringDto encounter)
         {
-            var result = _encountersService.Create(encounter);
-            return CreateResponse(result);
-        }
+            //    var result = _encountersService.Create(encounter);
+            //    return CreateResponse(result);
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string url = "http://localhost:8083/api/encounters";
+                    string jsonString = JsonConvert.SerializeObject(encounter);
+                    var response = await client.PostAsJsonAsync(url, encounter);
 
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response from server: " + responseContent);
+                        return CreateResponse(Result.Ok(response));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: " + response.StatusCode);
+                        return CreateResponse(Result.Fail("An error occurred"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.Message);
+                    return CreateResponse(Result.Fail("An error occurred").WithError(ex.Message));
+                }
+            }
+
+        }
+        // OVO MENJAM
+
+        //OVO MENJAM
         [HttpPut]
-        public ActionResult<EncounterDto> Update([FromBody] EncounterDto encounter)
+        public async Task<ActionResult<EncounterStringDto>> Update([FromBody] EncounterStringDto encounter)
         {
-            var result = _encountersService.Update(encounter);
-            return CreateResponse(result);
-        }
+            //    var result = _encountersService.Update(encounter);
+            //    return CreateResponse(result);
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
 
-        [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
-        {
-            var result = _encountersService.Delete(id);
-            return CreateResponse(result);
+                    string url = "http://localhost:8083/api/encounters/update/" + encounter.Id;
+                    string jsonString = JsonConvert.SerializeObject(encounter);
+                    var response = await client.PutAsJsonAsync(url, encounter);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response from server: " + responseContent);
+                        return CreateResponse(Result.Ok(response));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: " + response.StatusCode);
+                        return CreateResponse(Result.Fail("An error occurred"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.Message);
+                    return CreateResponse(Result.Fail("An error occurred").WithError(ex.Message));
+                }
+            }
         }
+        //OVO MENJAM
+
+        //OVO MENJAM
+        [HttpDelete("{id}")]
+        public async void Delete(string id)
+        {
+        //    var result = _encountersService.Delete(id);
+        //    return CreateResponse(result);
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "http://localhost:8083/api/encounters/" + id; // Change this URL as per your Spring Boot API endpoint
+                // Send a DELETE request to the specified URL
+                HttpResponseMessage response = await client.DeleteAsync(url);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("DELETE request successful");
+                }
+                else
+                {
+                    Console.WriteLine($"DELETE request failed with status code {response.StatusCode}");
+                }
+            }
+        }
+        //OVO MENJAM
     }
 }
---
+++ src/Explorer.Encounters.API/Dtos/EncounterStringDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Dtos
+{
+    public class EncounterStringDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public EncounterCoordinateDto? Coordinates { get; set; }
+        public int? Xp { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public EncounterStatusDto? Status { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public EncounterTypeDto? Type { get; set; }
+        public int? Range { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? MiscEncounterTask { get; set; }
+        public int? SocialEncounterRequiredPeople { get; set; }
+        public long? CheckpointId { get; set; }
+        public bool? IsRequired { get; set; }
+    }
+}
